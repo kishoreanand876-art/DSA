983662af2dfe61541775a82cdc702f106725a453
kishoreanand876-art <kishoreanand876@gmail.com>
2026-06-27 15:12:17 +0530

    feat: Add Number of Sub-arrays With Odd Sum

--- Array/Number of Sub-arrays With Odd Sum/java/Solution.java
+++ Array/Number of Sub-arrays With Odd Sum/java/Solution.java
@@ -1,21 +1,32 @@
+// class Solution {
+//     public int numOfSubarrays(int[] arr) {
+//         List<Integer> list=new ArrayList<>();
+//         for(int i=0;i<arr.length;i++){
+//             for(int j=i;j<arr.length;j++){
+//                 int sum=0;
+//                 for(int k=i;k<=j;k++){
+//                     sum+=arr[k];
+//                 }
+//                 list.add(sum);
+//             }
+//         }
+//         int count=0;
+//            for(int nums:list){
+//                if(nums % 2!=0){
+//                 count++;
+//                }
+//            }
+//         return count;
+//     }
+// }
 class Solution {
     public int numOfSubarrays(int[] arr) {
-        List<Integer> list=new ArrayList<>();
-        for(int i=0;i<arr.length;i++){
-            for(int j=i;j<arr.length;j++){
-                int sum=0;
-                for(int k=i;k<=j;k++){
-                    sum+=arr[k];
-                }
-                list.add(sum);
-            }
+        long oddCount = 0, prefixSum = 0;
+        for(int a : arr){
+            prefixSum += a;
+            oddCount += prefixSum % 2;
         }
-        int count=0;
-           for(int nums:list){
-               if(nums % 2!=0){
-                count++;
-               }
-           }
-        return count;
+        oddCount += (arr.length - oddCount) * oddCount;
+        return (int)(oddCount % 1_000_000_007);
     }
 }
